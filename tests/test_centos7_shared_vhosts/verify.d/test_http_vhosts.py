2add83a8ee4772189b1b84f799f0a4eef1e0153e
Marcin TORGiren Fabrykowski <torgiren@gmail.com>
2021-01-13 23:05:22 +0100

    add support for directories and aliases - #closes 167
    Signed-off-by: Marcin TORGiren Fabrykowski <torgiren@gmail.com>
    Change-Id: I27128d0f361e2f90fb860a39aeb4c8ff46944903

--- tests/test_centos7_shared_vhosts/verify.d/test_http_vhosts.py
+++ tests/test_centos7_shared_vhosts/verify.d/test_http_vhosts.py
@@ -29,3 +29,18 @@ def test_http_php_vhost3(host):
 def test_http_rewrite_vhost4(host):
     assert host.check_output("curl http://127.0.0.1:8084/wp -H 'Host: test-vhost4.example.com' -I -w '%{redirect_url}' -s -o /dev/null") == "https://wp.pl"
     assert host.check_output("curl http://127.0.0.1:8084/onet -H 'Host: test-vhost4.example.com' -I -w '%{redirect_url}' -s -o /dev/null") == "https://onet.pl"
+
+def test_http_directory_vhost4(host):
+    host.ansible(
+        "shell",
+        r"mkdir -p /app/httpd/httpd/var/www/test-vhost4/path1 && echo test_file1 > /app/httpd/httpd/var/www/test-vhost4/path1/file.txt",
+        become=True,
+        become_user='httpd',
+        check=False)
+    host.ansible(
+        "shell",
+        r"mkdir -p /srv/http/test && echo test_file2 > /srv/http/test/file.txt && chcon -t httpd_sys_rw_content_t /srv/http -R",
+        become=True,
+        check=False)
+    assert host.check_output("curl -H 'Host: test-vhost4.example.com' http://127.0.0.1:8084/path1/file.txt") == "test_file1"
+    assert host.check_output("curl -H 'Host: test-vhost4.example.com' http://127.0.0.1:8084/path2/file.txt") == "test_file2"
